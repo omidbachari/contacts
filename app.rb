37d7b0dd1849f5dd7f9d5a7d744a38dda04ead07
Omid Omid <omidbachari@gmail.com>
2014-06-06 20:49:46 -0400

    finished

--- app.rb
+++ app.rb
@@ -1,24 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
-
+require "sinatra/activerecord"
+require "pry"
 require_relative 'models/contact'
 
-before do
-  contact_attributes = [
-    { first_name: 'Eric', last_name: 'Kelly', phone_number: '1234567890' },
-    { first_name: 'Adam', last_name: 'Sheehan', phone_number: '1234567890' },
-    { first_name: 'Dan', last_name: 'Pickett', phone_number: '1234567890' },
-    { first_name: 'Evan', last_name: 'Charles', phone_number: '1234567890' },
-    { first_name: 'Faizaan', last_name: 'Shamsi', phone_number: '1234567890' },
-    { first_name: 'Helen', last_name: 'Hood', phone_number: '1234567890' },
-    { first_name: 'Corinne', last_name: 'Babel', phone_number: '1234567890' }
-  ]
 
-  @contacts = contact_attributes.map do |attr|
-    Contact.new(attr)
-  end
-end
 
 get '/' do
+  @contacts = Contact.all
   erb :index
+end
+
+get '/contacts/:id' do
+
+  @contact = Contact.find(params["id"])
+binding.pry
+  erb :shows
 end
